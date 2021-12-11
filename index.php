07bea083571817f66e69d22c5211ef18afbc9c15
juan6899 <90010849+juan6899@users.noreply.github.com>
2021-12-10 23:09:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IEA 20347</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/e494fb4b4c.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header class="main-header">
+        <nav class="header">
+            <div class="redes">
+                <ul><a href="https://www.facebook.com/home.php"><i class="fab fa-facebook"></i></a></ul>
+                <ul><a href="youtube.com"><i class="fab fa-youtube"></i></a></ul>
+                <ul><a href="#"><i class="fab fa-instagram"></i></a></ul>
+            </div>
+            <div class="contacto">
+                <ul>
+                    <li><a href="#"><i class="fas fa-phone-alt"></i>  +51987654321</a></li>
+                    <li><a href="#"><i class="fas fa-envelope"></i>  iea20347@gmail.com</a></li>
+                    <li><a href="#"><i class="fas fa-calendar-alt"></i>  Eventos</a></li>
+                </ul>
+
+            </div>
+        </nav>
+        <nav class="header-1">
+            <div><img src="Img/logo.jpg" alt=""></div>
+            <div class="menu-item">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#">¿Quienes Somos?</a>
+                        <ul>
+                            <li><a href="nosotros.html">Nosotros</a></li>
+                            <li><a href="mision.html">Mision y Vision</a></li>
+                            <li><a href="valores.html">Valores</a></li>
+                            <li><a href="historia.html">Historia</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#main-propuesta">Propuesta Educativa</a>
+                        <ul>
+                            <li><a href="#main-propuesta">Inicial</a></li>
+                            <li><a href="#main-propuesta">Primaria</a></li>
+                            <li><a href="#main-propuesta">Secundaria</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#main-testimonio">Testimonios</a>
+                        <ul>
+
+                            <li><a href="#main-testimonio">Docentes</a></li>
+                            <li><a href="#main-testimonio">Padres de Familia</a></li>
+                            <li><a href="#main-testimonio">Alumnos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="matricula.php">Admision</a>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section id="baner">
+        <div class="flex-center">
+
+                <h1>Institución Educativa Agropecuario N° 15</h1>
+                <p>Somos una institución educativa comprometida con la educación integral y emocional de todos sus
+                    estudiantes</p>
+                <div>
+                    <button id="btn-info">Mas información</button>
+                </div>
+
+        </div>
+
+    </section>
+
+    <section id="main-propuesta">
+        <div class=" content-1">
+            <div class="content-texto">
+                <h1>UNA EDUCACION DE CALIDAD Y CON VALORES</h1>
+            </div>
+
+            <div class="contenido-1">
+                <div class="cont-1">
+                    <h2>Educacion Inicial</h2>
+                    <img src="Img/edu_inicial.jpg" alt="">
+                    <div class="texto">
+                        <p>Es la base de la educación por ello nuestro objetivo principal es la formación integral del niño 
+                       a nivel socioemocional, biopsicomotor e intelectual, a través de su participación activa en los 
+                       contextos de juego teniendo un rol protagónico que le permite descubrir el mundo que lo rodea.</p>
+                    </div>
+                    
+                </div>
+                <div class="cont-2">
+                    <h2>Educacion Primaria </h2>
+                    <img src="Img/edu_primaria.jpg" alt="">
+                    <div class="texto">
+                        <p>Incentivamos sus capacidades académicas, sociales,  su pensamiento crítico, analítico y creativo 
+                       para desarrollar su formación científica y su carácter a fin de consolidar su personlidad y 
+                       enpoderarlo como un auténtico líder Huachano que responda a las exigencias y necesidades de la 
+                       sociedad.</p>
+                    </div>
+                    
+                </div>
+                <div class="cont-3">
+                    <h2>Educacion Secundaria</h2>
+                    <img src="Img/edu_secundaria.jpg" alt="">
+                    <div class="texto">
+                        <p> La enseñanza secundaria brinda formación básica para responder al fenómeno de la 
+                        universalización de la matrícula; preparar para la universidad pensando en quienes aspiran a 
+                        continuar sus estudios; preparar para el mundo del trabajo a los que no siguen estudiando y desean
+                        o necesitan incorporarse a la vida laboral</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+
+
+    </section>
+
+    <section id="mapa">
+        <div class="content-map">
+            <div class="content-texto">
+                <h1>UBICANOS</h1>
+            </div>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7830.739579728999!2d-
+            77.58632708465576!3d-11.085793171846543!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDA1JzA4LjYiUyA3N8KwMzUnMTAuOSJX
+            !5e0!3m2!1ses!2sus!4v1634105585461!5m2!1ses!2sus" width="80%" height="600px" style="border:0;"
+                allowfullscreen="" loading="lazy"></iframe>
+        </div>
+
+    </section>
+
+    <section id="main-testimonio">
+        <div class=" content-2">
+            <div class="content-texto">
+                <h1>DOCENTES, ALUMNOS Y PADRES DE FAMILIA NOS CUENTAN SUS EXPERIENCIAS </h1>
+            </div>
+
+            <div class="contenido-2">
+                <div class="cont-1">
+                    <h2>Maestros</h2>
+                    <video src="video/video-1.mp4" controls></video>
+                    <p>Esto es un grupo de bloques en varias columnas. El diseño de las columnas puede adaptarse según
+                        distintas necesidades.</p>
+                </div>
+                <div class="cont-2">
+                    <h2>Alumnos</h2>
+                    <video src="video/video-2.mp4" controls></video>
+                    <p>Esto es un grupo de bloques en varias columnas. El diseño de las columnas puede adaptarse según
+                        distintas necesidades.</p>
+                </div>
+                <div class="cont-3">
+                    <h2>Padres de Familia</h2>
+                    <video src="video/video-3.mp4" controls></video>
+                    <p>Esto es un grupo de bloques en varias columnas. El diseño de las columnas puede adaptarse según
+                        distintas necesidades.</p>
+                </div>
+            </div>
+
+        </div>
+
+
+    </section>
+
+    <section id="main-contactenos">
+
+        <form action="" method="post" enctype="text/plain">
+            <h1>Deja un mensaje</h1>
+            <div class="form">
+                <input type="text" name="Nombre" placeholder="  Tu Nombre">
+                <input type="text" name="Correo" placeholder="  Tu Correo">
+                <textarea placeholder="  Escriba su Mensaje" name="Mensaje" id="" cols="30" rows="10"></textarea>
+                <button id="enviar">Enviar Mensaje</button>
+            </div>
+
+        </form>
+
+    </section>
+
+
+    <footer id="main-footer">
+        <p>&copy; 2021 <a href="#">IEA20347.edu.pe</a></p>
+    </footer>
+
+</body>
+
+</html>
